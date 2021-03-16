dbc48809c014f9455df3bf70dbccf748caca58f9
DMUPraveen <dmu.praveen@gmail.com>
2021-03-16 20:19:48 +0530

    Created ZigZag

--- SpiralOrZigZag.py
+++ SpiralOrZigZag.py
@@ -55,9 +55,78 @@ def createSpiralPath(columns,rows,startCorner,direction):
 
 
 def createZigZagPath(columns,rows,startCorner,direction):
+    '''
+    startCorner should be 0,1,2,3
+    0      1
+
+    3      2
+
+    direction is 0 or 1
+    1 left-right
+    -1 updown
+
+    '''
+    path = []
+    grid = Grid(rows,columns)
+    corners= [0, columns-1,rows*columns-1,rows*columns - columns]
+    x,y = grid.getXY(corners[startCorner])
+
+    if(direction == 1):
+        shiftDirections = [
+            Robot.Directions["S"],
+            Robot.Directions["S"],
+            Robot.Directions["N"],
+            Robot.Directions["N"]
+            ]
+
+        startOrientation = [
+            (Robot.Directions["E"],Robot.Directions["W"]),
+            (Robot.Directions["W"],Robot.Directions["E"]),
+            (Robot.Directions["W"],Robot.Directions["E"]),
+            (Robot.Directions["E"],Robot.Directions["W"])
+            ]
+
+    else:
+        shiftDirections = [
+            Robot.Directions["E"],
+            Robot.Directions["W"],
+            Robot.Directions["W"],
+            Robot.Directions["E"]
+            ]
+
+        startOrientation = [
+            (Robot.Directions["S"],Robot.Directions["N"]),
+            (Robot.Directions["S"],Robot.Directions["N"]),
+            (Robot.Directions["N"],Robot.Directions["S"]),
+            (Robot.Directions["N"],Robot.Directions["S"])
+            ]
+
+    
+    robot = Robot(grid,x,y)
+    dirIndex = 0
+    dirTuple = startOrientation[startCorner]
+    shiftDirection = shiftDirections[startCorner]
+    while(grid.cleanedTiles != grid.UnblockedTiles):
+        path.append(grid.Id(robot.X,robot.Y))
+        state = robot.getNeighbourSquareABS(dirTuple[dirIndex])
+        if(state == grid.states["FREE"]):
+            robot.goToNeighboutSquareABS(dirTuple[dirIndex])
+        else:
+        
+            state = robot.getNeighbourSquareABS(shiftDirection)
+            if(state != grid.states["FREE"]):
+                break
+            robot.goToNeighboutSquareABS(shiftDirection)
+            dirIndex  = (dirIndex+1)%2
+
+    path.append(grid.Id(robot.X,robot.Y))
+    replay(Grid(rows,columns),robot.moves,x,y,0)
+    return path
+
     
 
-        
+if __name__ == "__main__":
+    print(createZigZagPath(5,5,3,-1))
 
 
     
